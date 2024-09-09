5acbb70940c0ae05922211fad17cfa4fac8b0a21
Riccardo Pastori <ricpastori@icloud.com>
2024-09-09 20:04:45 +0200

    Updated the App.elm and index.html files to use elm ports and deconstruct the application so that elm renders only the interactive elements of the application and not the entire application

--- src/App.elm
+++ src/App.elm
@@ -1,10 +1,10 @@
-module App exposing (..)
+port module App exposing (..)
 
 import Browser
-import Char exposing (Char)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Char exposing (fromCode)
 import Round as R exposing (round)
 
 
@@ -15,9 +15,18 @@ import Round as R exposing (round)
 unicodeToString : Int -> String
 unicodeToString codePoint =
     codePoint
-        |> Char.fromCode
+        |> fromCode
         |> String.fromChar
 
+
+-- PORTS
+port reset : (String -> msg) -> Sub msg
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    reset Reset
 
 
 -- MODEL
@@ -31,13 +40,13 @@ type alias Model =
     }
 
 
-initModel : Model
-initModel =
-    { enteredWeight = ""
+init : () -> ( Model, Cmd Msg )
+init () =
+    ({ enteredWeight = ""
     , cannedTaurine = 0.0
     , dryTaurine = 0.0
     , message = ""
-    }
+    }, Cmd.none)
 
 
 
@@ -46,10 +55,11 @@ initModel =
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initModel
+    Browser.element
+        { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
 
 
@@ -60,9 +70,7 @@ main =
 view : Model -> Html Msg
 view model =
     div [ class "flex flex-col justify-start gap-6" ]
-        [ h1 [ class "text-2xl font-bold" ] [ text "Taurine intake calculator" ]
-        , p [] [ text "This calculator is designed for healthy adult cats and provides an approximate estimate of their daily taurine requirement." ]
-        , div [ class "flex flex-col justify-start gap-1" ]
+        [ div [ class "flex flex-col justify-start gap-1" ]
             [ label [ class "font-semibold text-sm" ] [ text "Cat's weight" ]
             , div [class "flex flex-row flex-nowrap justify-start gap-2"] [input
                 [ type_ "text"
@@ -91,9 +99,6 @@ view model =
             , h2 [ class "font-semibold" ] [ text "Minimum intake for a dry diet:" ]
             , p [] [ em [] [ text (R.round 2 model.dryTaurine) ], text " mg of taurine per day" ]
             ]
-        , p [ class "text-sm"] [b [] [text "Source: "],a [ href "https://www.cambridge.org/core/services/aop-cambridge-core/content/view/BA66B9E895186245E2B050CC8F3ED14C/S0007114591000843a.pdf/effect_of_dietary_taurine_content_on_the_plasma_taurine_concentration_of_the_cat.pdf",target "_blank", rel "noopener", class "underline text-orange-500 hover:text-orange-600"] [ text "Earle, K. E., & Smith, P. M. (1991). The effect of dietary taurine content on the plasma taurine concentration of the cat. British Journal of Nutrition, 66(2), 227-235." ]]
-        , p [] [ b [] [ text "N.B." ], text "These results are only for guidance. It's always a good idea to check with a trusted veterinarian to make sure your cat is happy and healthy." ]
-        , button [ class "group physical secondary", onClick Reset ] [ span [ class "physical" ] [ text "Reset" ] ]
         ]
 
 
@@ -104,14 +109,14 @@ view model =
 type Msg
     = UpdateWeight String
     | Calculate
-    | Reset
+    | Reset String
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         UpdateWeight weight ->
-            { model | enteredWeight = weight }
+            ({ model | enteredWeight = weight }, Cmd.none)
 
         Calculate ->
             let
@@ -120,14 +125,14 @@ update msg model =
             in
             case maybeFloat of
                 Just f ->
-                    { model
+                    ({ model
                         | cannedTaurine = f * 39.0
                         , dryTaurine = f * 19.0
                         , message = ""
-                    }
+                    }, Cmd.none)
 
                 Nothing ->
-                    { model | message = unicodeToString 0x26A0 ++ " The entered value is not a number" }
+                    ({ model | message = unicodeToString 0x26A0 ++ " The entered value is not a number" }, Cmd.none)
 
-        Reset ->
-            initModel
+        Reset _ ->
+           init ()
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { Elm } from './App.elm'
 
-Elm.App.init({
+const app = Elm.App.init({
   node: document.getElementById("app")
+});
+
+document.getElementById('reset-button').addEventListener('click', function() {
+  app.ports.reset.send("reset");
 });
 
